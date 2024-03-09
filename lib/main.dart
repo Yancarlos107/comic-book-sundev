a2dc0353682e07a23e45b19615d73567e0cab2c0
Yan Carlos <yancarlosrt842@gmail.com>
2024-03-09 00:12:10 -0500

    firebase initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:comic_book/firebase_options.dart';
 import 'package:comic_book/pages/pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
